793c7b3e704639dd4f782a8223d72715cdd2bab2
Brent Barbachem <bbarbach@redhat.com>
2022-04-22 11:27:37 -0400

    Generation of the Dockerfile and thus an image creation for openshift installer.

--- container/installer/generate.py
+++ container/installer/generate.py
@@ -1,20 +1,19 @@
 import argparse
 from logging import getLogger, DEBUG
 from getpass import getuser
-from os.path import isdir, isfile, exists
-from os import mkdir, chmod, stat
-from datetime import datetime
-from shutil import rmtree
+from os.path import isdir, isfile, exists, dirname, abspath, join
+from os import mkdir, chmod, stat, listdir
+from shutil import rmtree, copy
 import stat as STAT
 from pathlib import Path
 from yaml import safe_load
+from jinja2 import Environment, FileSystemLoader, Template
 
 
 log = getLogger()
 log.setLevel(DEBUG)
-
-
 generation_dir = "openshift-installer-data"
+
 
 def get_custom_packages(platform):
     """
@@ -26,7 +25,7 @@ def get_custom_packages(platform):
     }.get(platform, [])
 
 
-def generate_dockerfile_statements(platform):
+def generate_commands(platform):
     """
     A cloud platform may require additional packages or commands. Add
     a list of those here for the platform.
@@ -35,90 +34,13 @@ def generate_dockerfile_statements(platform):
 
     if platform == "gcp":
         statements.extend([
-            'RUN curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-382.0.0-linux-x86_64.tar.gz',
-            'RUN -xf google-cloud-cli-382.0.0-linux-x86.tar.gz',
-            'RUN ./google-cloud-sdk/install.sh'
+            'curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-382.0.0-linux-x86_64.tar.gz',
+            'tar -xf google-cloud-cli-382.0.0-linux-x86.tar.gz',
+            './google-cloud-sdk/install.sh'
         ])
 
     return statements
 
-
-def generate_dockerfile(packs, envvars, extra_commands):
-    """
-    Generate the docker file.
-
-    :param packs: List of packages to install
-    :param envvars: list of environment variables to add to the image
-    :param extra_commands: List of extra commands added to the end of the file, see `generate_dockerfile_statements`
-    """
-    init_dockerfile_data = """from fedora:latest
-
-# Not building secrets and keys into the DOCKERFILE so that the
-# user is forced to copy their own over in the future. These values
-# can change and thus should not be added here even in a generic fashion
-
-# update and install new packages
-RUN yum update -y
-RUN yum install -y {}
-
-# Grab the openshift client, unpack it and move it to the bin for use
-RUN wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz && tar -xvzf openshift-client-linux.tar.gz && cp oc /usr/bin/
-
-# set the cluster directory where the information will be used for the installer
-ADD install.sh /install.sh
-RUN mkdir /cluster
-ADD install-config.yaml /cluster/install-config.yaml
-
-# added environment variables
-{}
-
-{}
-    """.format(
-        " ".join(packs),
-        "\n".join(envvars),
-        "\n".join(extra_commands)
-    )
-
-    with open("{}/Dockerfile".format(generation_dir), "w+") as dfile:
-        dfile.write(init_dockerfile_data)
-
-
-def generate_install_config(cluster_name, platform, region, ssh_key, secrets):
-    """
-    Generate an install-config, similar to running `openshift-installer create install-config`
-
-    :param cluster_name: name of the cluster
-    :param platform: cloud platform in use
-    :param region: region from that platform
-    :param ssh_key: data from the public ssh key file
-    :param secrets: data from the secrets file.
-    """
-    install_config_data = """apiVersion: v1
-baseDomain: devcluster.openshift.com
-compute:
-- architecture: amd64
-  hyperthreading: Enabled
-  name: worker
-  replicas: 3
-controlPlane:
-  architecture: amd64
-  hyperthreading: Enabled
-  name: master
-  replicas: 3
-metadata:
-  name: {}
-platform:
-  {}:
-    region: {}
-publish: External
-pullSecret: '{}'
-sshKey: |
-  {}
-"""
-
-    with open("{}/install-config.yaml".format(generation_dir), 'w+') as install_config:
-        install_config.write(install_config_data.format(cluster_name, platform, region, secrets, ssh_key))
-        
 
 def generate_install_in_container_script():
     # generate a script that will be added to the container
@@ -133,28 +55,51 @@ def generate_install_in_container_script():
     p = Path(script_name)
     p.chmod(p.stat().st_mode | STAT.S_IEXEC) 
 
-def generate_install_base_install_script(platform, os_installer_dir, os_image_name, custom_image_name, custom_image_tag, date_str):
+
+def generate_configure_in_container_script(packs, envvars, commands):
+    # generate a script that will be added to the container
+    data = """#!/bin/bash
+
+yum install -y {}
+
+{}
+
+{}
+    """
+    for i in range(len(envvars)):
+        if not envvars[i].startswith("export"):
+            envvars[i] = "export " + envvars[i]
+    
+    data.format(" ".join(packs), "\n".join(envvars), "\n".join(commands))
+    
+    script_name = "{}/configure.sh".format(generation_dir)
+    
+    with open(script_name, "w+") as ifile:
+        ifile.write(data)
+
+    p = Path(script_name)
+    p.chmod(p.stat().st_mode | STAT.S_IEXEC) 
+
+    
+def generate_install_base_install_script(platform, os_installer_dir, os_image_name, custom_image_name, custom_image_tag):
     # generate the script that will be used to create a container from the image
     data = """#!/bin/bash
 
 set -eux
 
-src_dir={}
-installer_binary=$src_dir/bin/openshift-install
-    
-#    -v "/home/{}/clusters/{}/{}":/cluster/        \\
+installer_binary={}/bin/openshift-install
+
 podman run --rm -it                               \\
     -v $installer_binary:/openshift-install    \\
     -v "/home/{}/.{}":/root/.{}                         \\
     -e OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE={} \\
-    -e KUBECONFIG="/c/auth/kubeconfig"             \\
+    -e KUBECONFIG="/cluster/auth/kubeconfig"             \\
     {}:{} /bin/bash
     """.format(
-        os_installer_dir, # pre podman-command  
-        getuser(), platform, date_str, # line 2
-        getuser(), platform, platform, # line 4
-        os_image_name, # line 5
-        custom_image_name, custom_image_tag # line 7
+        os_installer_dir, # pre podman-command
+        getuser(), platform, platform, # line 2
+        os_image_name, # line 3
+        custom_image_name, custom_image_tag # line 5
     )
 
     script_name = "{}/connect.sh".format(generation_dir)
@@ -176,13 +121,13 @@ if __name__ == "__main__":
     if exists(ssh_file) and isfile(ssh_file):
         with open(ssh_file, 'r') as s:
             ssh_info = s.read()
+
+        if ssh_info is None:
+            log.error("No ssh info found")
+            exit(1)
     else:
         log.error("Invalid ssh key file")
         exit(1)
-
-    if ssh_info is None:
-        log.error("No ssh info found")
-        exit(2)
 
     secrets_file = yaml_data["secrets_file"]
     # read in the secrets data from the file
@@ -190,23 +135,57 @@ if __name__ == "__main__":
     if exists(secrets_file) and isfile(secrets_file):
         with open(secrets_file, 'r') as s:
             secrets_info = s.read()
+
+        if secrets_info is None:
+            log.error("No secrets info found")
+            exit(2)
     else:
         log.error("Invalid secrets file")
-        exit(3)
+        exit(2)
 
-    if secrets_info is None:
-        log.error("No secrets info found")
-        exit(4)
-
+    # reebuild the generation directory where the data should reside
+    if isdir(generation_dir):
+        log.warning("Removing all data from {}".format(generation_dir))
+        rmtree(generation_dir)
+        
+    # make the directory where everything will go
+    log.debug("Creating {}".format(generation_dir))
+    mkdir(generation_dir)
+        
+    # Grab the information from the YAML File
     platform = yaml_data["platform"]
     region = yaml_data["region"]
-
-
+    base_domain = yaml_data["base_domain"]
     installer_dir = yaml_data["installer"]["dir"]
     os_image = yaml_data["installer"]["image"]
-
     docker_image_name = yaml_data["docker"]["image_name"]
     docker_image_tag = yaml_data["docker"]["tag"]
+
+    # generate the cluster name
+    cluster_name = "{}-test-{}".format(getuser(), platform)
+
+    jinja_data = {}
+    jinja_data.update(yaml_data)
+    jinja_data["cluster_name"] = cluster_name
+    jinja_data["pull_secret"] = secrets_info
+    jinja_data["ssh_key"] = ssh_info
+    
+    # using the template and the config data to generate the install-config
+
+    p = dirname(abspath(__file__)) + "/../templates/{}/".format(platform)
+    j2_files = [dirf for dirf in listdir(p) if isfile(join(p, dirf)) and dirf.endswith(".j2")]
+    if j2_files:
+        j2_file = j2_files[0]
+        log.debug("Using template: {}".format(j2_file))
+    else:
+        log.error("No .j2 templates found")
+        exit(3)
+
+    with open(join(p, j2_file), "r") as jfile:
+        template = Template(jfile.read())
+    output = template.render(jinja_data)
+    with open("{}/{}".format(generation_dir, j2_file.replace(".j2", "")), "w+") as ic:
+        ic.write(output)
     
     creds_dir = "/home/{}/.{}".format(getuser(), platform)
     log.debug("Set the credentials directory to {}".format(creds_dir))
@@ -214,51 +193,18 @@ if __name__ == "__main__":
     if not isdir(creds_dir):
         log.error("{} does not exist".format(creds_dir))
         exit(1)
-
-    log.debug("Region is set to {}".format(region))
-
-    # reebuild the generation directory where the data should reside
-    if isdir(generation_dir):
-        log.warning("Removing all data from {}".format(generation_dir))
-        rmtree(generation_dir)
         
-    mkdir(generation_dir)
-    # create the dir where extra scripts will reside
-    mkdir("{}/cluster".format(generation_dir))
-
-    # datetime string that can be used for several functions if needed
-    # this will control the name of the clusters and directories
-    dt = datetime.now().strftime("%Y%m%d")
-
-    generate_install_config(
-        "{}-{}-{}".format(getuser(), platform, dt),
-        platform,
-        region,
-        ssh_info,
-        secrets_info
-    )
-
     generate_install_in_container_script()
-    generate_install_base_install_script(
-        platform,
-        installer_dir,
-        os_image,
-        docker_image_name,
-        docker_image_tag,
-        dt
-    )
+    generate_install_base_install_script(platform, installer_dir, os_image, docker_image_name, docker_image_tag)
 
-    # base packages + platform specific
-    packs = ["jq", "wget", "ncurses"] + get_custom_packages(platform)
+    packs = get_custom_packages(platform)
+    envvars = ["{}={}".format(k, v) for k, v in yaml_data["env"].items()]
+    commands = generate_commands(platform)
+    generate_configure_in_container_script(packs, envvars, commands)
 
-    # supply everything that we have, doesn't matter on the platform, the function
-    # will choose what it needs
-    envvars = ["ENV {}={}".format(k, v) for k, v in yaml_data["env"].items()]
-
-    extra_df_statements = generate_dockerfile_statements(platform)
-
-    generate_dockerfile(packs, envvars, extra_df_statements)
+    # copy the dockerfile over to the openshift-install-dir
+    log.debug("Copying Dockerfile over to {}".format(generation_dir))
+    copy(join(dirname(abspath(__file__)), "Dockerfile"), generation_dir)
     
-
     print("Execute the following command:\n")
     print("cd {} && podman build . -t {}:{}".format(generation_dir, docker_image_name, docker_image_tag))
--- container/openshift-ansible/README.md
+++ container/openshift-ansible/README.md
@@ -7,30 +7,54 @@ Throughout the document, AWS will be the platform of choice.
 
 See the following Projects for references that will be mentioned throughout this document:
 
-[OI-Dev](https://github.com/jstuever/oi-dev)
-[OA-Testing](https://github.com/mtnbikenc/oa-testing)
-[Openshift Installer](https://github.com/openshift/installer)
-[Openshift Ansible](https://github.com/openshift/openshift-ansible)
+- [Pull Secrets](./PullSecrets.md)
+- [OA-Testing](https://github.com/mtnbikenc/oa-testing) - Utilities to install clusters to aws utilizing openshift-ansible
+- [OI-Dev](https://github.com/jstuever/oi-dev) - Simpler cluster installation utilizing openshift-ansible
+- [Openshift Installer](https://github.com/openshift/installer)
+- [Openshift Ansible](https://github.com/openshift/openshift-ansible)
+
 
 # Process
 
-1. Create a cluster using openshift-installer.
+1. Make a directory called `assets` in `oi-dev`
+
+The assets directory is the default where the installation should occur for the `oi-dev` project to
+utilize openshift-installer information.
+
+2. Create a cluster using openshift-installer.
 
 ```bash
 cd /path/to/installer/;
 
 hack/build.sh;
 
-bin/openshift-installer create cluster
+bin/openshift-installer create cluster --dir /path/to/assets
 
 ```
 
 Go through the process of creating a cluster for AWS.
 
 
-2. Ensure that there is an ssh key called `oi`.
+3. Ensure that there is an ssh key called `oi`.
 
 There should be a matching public key called `oi.pub`. It is ok to **copy** the
 ssh key that you normally use. DO NOT sim-link the keys as this could create issues if you
 ever remove the keys or change them.
 
+
+4. Add the openshift-installer bin to the path
+
+`export PATH=/path/to/installer/bin:$PATH`
+
+For me this would include
+
+`export PATH=$HOME/dev/installer/bin:$PATH`
+
+
+# FAQ
+
+1. What is a bastion?
+
+The bastion was/is a defense mechanism that provides a link or connection to the cluster. An `ssh pod` or bastion is installed
+in the cluster so that we can reach the cluster through this pod. It provides a connection to the cluster but access must be
+achieved indirectly.
